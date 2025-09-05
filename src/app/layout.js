901070aafda91d351b32a5e41fb10aec1dbbd87d
Mahfuj Abdulla <155483252+IamFreak10@users.noreply.github.com>
2025-09-05 16:29:49 +0600

    Banner.jsx updated /rewrited

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import "./globals.css";
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -20,11 +21,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="light">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar/>
+        <Navbar />
         {children}
         <Footer/>
       </body>
