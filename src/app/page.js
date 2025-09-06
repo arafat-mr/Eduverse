35759a30a43f37614463f58f6aaecbaa671e2ada
Mohima Chowdhury <work.mohima@gmail.com>
2025-09-06 13:28:02 +0600

    Resolve merge conflict in Navbar.jsx and update dependencies

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 import Banner from "./components/Banner";
+import Chatbot from "./components/ChatBot";
 import CoursesPage from "./components/courses/CoursesPage";
 import HomeCourse from "./components/HomeCourse";
 
 export default function Home() {
   return (
     <div>
+
       <Banner/>
+      <Chatbot/>
       <HomeCourse/>
       <CoursesPage/>
     </div>
