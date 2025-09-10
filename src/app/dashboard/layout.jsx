3a363dee4d7d4d2f114d6b3c794485e3063a71c8
Mahfuj Abdulla <155483252+IamFreak10@users.noreply.github.com>
2025-09-10 21:41:29 +0600

    Saved Code From.......

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,20 +1,7 @@
 "use client";
 
-<<<<<<< HEAD
-const adminLinks = [
-  { href: "/dashboard", label: "Dashboard" },
-  { href: "/dashboard/profile", label: "Profile Management" },
-  { href: "/dashboard/addcourse", label: "Add Courses" },
-  { href: "/dashboard//release-module", label: "Release Module" },
-  { href: "/dashboard/students", label: "Student Management" },
-  { href: "/dashboard/payments", label: "Payment Management" },
-  { href: "/dashboard/certificates", label: "Certificates" },
-  { href: "/dashboard/my-enrollments", label: "My Enrollments" },
-];
-=======
 import React from "react";
 import AdminLayout from "../components/AdminLayout";
->>>>>>> 6122c5fd38389123355318ae810f9d13e67f0ef5
 
 export default function DashboardLayout({ children }) {
   return <AdminLayout>{children}</AdminLayout>;
